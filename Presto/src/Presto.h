18815193e9b7a66b19ed5929ebd95955c0c0a76b
Adrian Hardy Hansen <hansen.adrian.hardy@gmail.com>
2021-10-18 03:12:52 +0200

    Added logging

--- Presto/src/Presto.h
+++ Presto/src/Presto.h
@@ -1,4 +1,5 @@
 #pragma once
 
 #include "Presto/Application.h"
+#include "Presto/Log.h"
 #include "Presto/EntryPoint.h"
--- Presto/src/Presto/EntryPoint.h
+++ Presto/src/Presto/EntryPoint.h
@@ -6,6 +6,8 @@ extern Presto::Application* Presto::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Presto::Log::Init();
+	PRESTO_CORE_WARN("Presto!!");
 	Presto::Application* app = Presto::CreateApplication();
 	app->Run();
 	delete app;
