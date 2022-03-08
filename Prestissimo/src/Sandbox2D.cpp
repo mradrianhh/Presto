d617fd184fa25021decb902055595438a1723bff
Adrian Hardy Hansen <hansen.adrian.hardy@gmail.com>
2022-03-08 06:30:48 +0100

    Added rotated batch rendering.

--- Prestissimo/src/Sandbox2D.cpp
+++ Prestissimo/src/Sandbox2D.cpp
@@ -45,6 +45,7 @@ void Sandbox2D::OnUpdate(Presto::Timestep ts)
 		Presto::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Presto::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 		Presto::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Presto::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, 45, m_CheckerboardTexture);
 		Presto::Renderer2D::EndScene();
 	}
 }
